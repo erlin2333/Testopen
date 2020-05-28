30fc9b18dbaab4622a7cdff295bed9d47377de3f
erlin2333 <1661131247@qq.com>
2020-05-28 20:39:04 +0800

    testng注解@Test标签

---
+++ Autotest1/src/main/java/com/autotest/testng/BasicAnnotation.java
@@ -0,0 +1,11 @@
+package com.autotest.testng;
+
+import org.testng.annotations.Test;
+
+public class BasicAnnotation {
+
+    @Test
+    public void testCase1(){
+        System.out.println("测试用例1");
+    }
+}
